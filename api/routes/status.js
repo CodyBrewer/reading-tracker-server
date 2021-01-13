2cad1771a93532e35a9a26e9d07e5c4f4b9f1b7b
Cody Brewer <codybrewerdev@gmail.com>
2021-01-13 16:22:53 -0700

    feat(status.js): get /status endpoint implemented

---
+++ api/routes/status.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+/**
+ * @swagger
+ * /status:
+ *  get:
+ *    description: root path returning status
+ *    tags:
+ *      - status
+ *    responses:
+ *      200:
+ *        description: status is up
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: Object
+ *              properties:
+ *                message:
+ *                  type: String
+ *                current_time:
+ *                  type: String
+ *                  description: Current Date Time in UTC
+ *              example:
+ *                {
+ *                  "message": "up",
+ *                  "current_time": "Wed, 13 Jan 2021 23:04:22 GMT"
+ *                }
+ */
+router.get('/', (req, res) => {
+  const currentTime = new Date(Date.now());
+  res.send({ message: 'up', current_time: currentTime.toUTCString() });
+});
+
+module.exports = router;
