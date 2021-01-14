55666af1adb7cb31cbc9d4eeabe0aba26835a189
Cody Brewer <codybrewerdev@gmail.com>
2021-01-13 21:15:27 -0700

    feat(knexfile.js): set up knex config

---
+++ config/knexfile.js
@@ -0,0 +1,28 @@
+require('dotenv').config({ path: '../.env' });
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+  },
+};
