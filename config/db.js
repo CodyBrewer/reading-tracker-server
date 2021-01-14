2d9bfdd5c2e29f0821473620fbedace36179585a
Cody Brewer <codybrewerdev@gmail.com>
2021-01-14 13:24:52 -0700

    feat(db.js): database configuration file for interacting with the database via model functions

---
+++ config/db.js
@@ -0,0 +1,8 @@
+require('dotenv').config({ path: '../.env' });
+
+const knex = require('knex');
+
+const dbEnv = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile')[dbEnv];
+
+module.exports = knex(knexConfig);
