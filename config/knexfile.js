e4eb336963042383e83173e104906210ec3283e3
Cody Brewer <codybrewerdev@gmail.com>
2021-01-15 17:58:48 -0700

    didnt put connection details into connection

--- config/knexfile.js
+++ config/knexfile.js
@@ -19,12 +19,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    ssl: true,
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: true,
+    },
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
   },
