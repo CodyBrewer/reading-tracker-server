8dd9bd46af41ba5c11464e34eb8879f2eea7f411
Cody Brewer <codybrewerdev@gmail.com>
2021-01-19 12:45:48 -0700

    refactor(create_users.js): remove linting error for expected linebreak

--- data/migrations/20210113212636_create_users.js
+++ data/migrations/20210113212636_create_users.js
@@ -1,11 +1,12 @@
-exports.up = (knex) =>
+exports.up = (knex) => {
   knex.schema.createTable('users', (table) => {
-    table.increments('id')
-    table.uuid('uuid').unique().notNullable()
-    table.string('username').notNullable().unique()
-    table.string('email').notNullable().unique()
-    table.string('password').notNullable()
-    table.string('avatar_url').notNullable()
-  })
+    table.increments('id');
+    table.uuid('uuid').unique().notNullable();
+    table.string('username').notNullable().unique();
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    table.string('avatar_url').notNullable();
+  });
+};
 
 exports.down = (knex) => knex.schema.dropTableIfExists('users')
