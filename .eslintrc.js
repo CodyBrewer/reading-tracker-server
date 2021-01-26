e17c70ac449726eb6351999a6ef00cf2d494feb6
Cody Brewer <codybrewerdev@gmail.com>
2021-01-26 09:57:57 -0700

    build(.eslintrc.js): switch style guide from airbnb to standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,14 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
-  overrides: [
-    {
-      files: ['__tests__/*', '__tests__/**/*'],
-      env: {
-        jest: true,
-      },
-    },
+  extends: [
+    'standard'
   ],
-};
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  }
+}
