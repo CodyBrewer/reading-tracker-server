db7dc4794234a1bfca5e19f01deba331f76d0a2d
Cody Brewer <codybrewerdev@gmail.com>
2021-01-26 12:22:08 -0700

    remove semicolons

--- server.js
+++ server.js
@@ -7,16 +7,10 @@ const swaggerJSDoc = require('swagger-jsdoc')
 const jsdocConfig = require('./config/jsdoc')
 const { notFound, errorHandler } = require('./api/middleware/error.middleware')
 // ###[Routers]###
-<<<<<<< HEAD
-const statusRouter = require('./api/routes/status.router');
-const authenticationRouter = require('./api/routes/authentication.router');
-const profileRouter = require('./api/routes/profiles.router');
-const readingListRouter = require('./api/routes/readingLists.router');
-=======
 const statusRouter = require('./api/routes/status.router')
 const authenticationRouter = require('./api/routes/authentication.router')
 const profileRouter = require('./api/routes/profiles.router')
->>>>>>> 72848f967cee534c73cd34a37d6ddebf36e400b5
+const readingListRouter = require('./api/routes/readingLists.router')
 
 const swaggerSpec = swaggerJSDoc(jsdocConfig)
 const swaggerUIOptions = {
@@ -33,21 +27,21 @@ server.use(
   })
 )
 server.use(express.json())
-server.use(process.env.NODE_ENV === 'production' ? logger('common') : logger('dev'))
+server.use(
+  process.env.NODE_ENV === 'production' ? logger('common') : logger('dev')
+)
 
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUIOptions))
+server.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerUIOptions)
+)
 
 // application routes
-<<<<<<< HEAD
-server.use('/status', statusRouter);
-server.use('/auth', authenticationRouter);
-server.use('/profiles', profileRouter);
-server.use('/readingLists', readingListRouter)
-=======
 server.use('/status', statusRouter)
 server.use('/auth', authenticationRouter)
 server.use('/profiles', profileRouter)
->>>>>>> 72848f967cee534c73cd34a37d6ddebf36e400b5
+server.use('/readingLists', readingListRouter)
 
 // 404 not found middleware
 server.use(notFound)
