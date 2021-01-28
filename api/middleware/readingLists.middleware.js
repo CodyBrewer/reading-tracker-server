e5fb6494349d084bf951fdfc54d1840668e2ff60
Cody Brewer <codybrewerdev@gmail.com>
2021-01-28 12:39:08 -0700

    feat(readingLists.middleware.js): verifyBody, verifyBook, verifyAuthors, verifyAuthorBook middleware to run on post readingLists/:id
    verifyBook, verifyAuthors,verifyAuthorBook check if the data in the request body matches any entries in the database and if not will create them so reading_list data is correct

---
+++ api/middleware/readingLists.middleware.js
@@ -0,0 +1,122 @@
+const BooksModel = require('../models/books.model')
+const AuthorsModel = require('../models/authors.model')
+const AuthorBooksModel = require('../models/authorBooks.model')
+
+const requiredBookFields = [
+  'google_id',
+  'title',
+  'cover_image',
+  'description',
+  'page_count',
+  'authors'
+]
+
+const verifyBody = (req, res, next) => {
+  requiredBookFields.forEach((property) => {
+    if (!Object.prototype.hasOwnProperty.call(req.body, property)) {
+      const error = new Error()
+      error.statusCode = 400
+      error.message = `Missing Required property: ${property}`
+      next(error)
+    }
+  })
+  next()
+}
+
+const verifyBook = async (req, res, next) => {
+  const { google_id, title, cover_image, description, page_count } = req.body
+  try {
+    let book = await BooksModel.getBy({ google_id }).first()
+    if (book) {
+      res.locals.book = book
+      next()
+    } else {
+      try {
+        book = await BooksModel.create({
+          google_id,
+          title,
+          cover_image,
+          description,
+          page_count
+        })
+        if (book !== null) {
+          res.local.book = insertedBook
+          next()
+        }
+      } catch (error) {
+        error.statusCode = 500
+        error.message = 'Error creating new book in database'
+        next(error)
+      }
+    }
+  } catch (error) {
+    error.statusCode = 500
+    error.message = 'Error checking if the book exists in the database'
+    next(error)
+  }
+}
+
+const verifyAuthors = async (req, res, next) => {
+  if (req.body.authors.length > 1) {
+    try {
+      const authors = await Promise.all(
+        req.body.authors.map(async (author) => {
+          let authorCheck = await AuthorsModel.get({ name: author })
+          if (!authorCheck) {
+            authorCheck = await AuthorsModel.create({ name: author })
+          }
+          return authorCheck
+        })
+      )
+      res.locals.authors = authors
+      next()
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    try {
+      let author = await AuthorsModel.get({ name: req.body.authors[0] })
+      if (!author) {
+        author = await AuthorsModel.create({ name: req.body.authors[0] })
+      }
+      res.locals.authors = author
+      next()
+    } catch (error) {
+      error.statusCode = 500
+      error.message = 'Error checking if Authors exist in database'
+      next(error)
+    }
+  }
+}
+
+const verifyAuthorBook = async (req, res, next) => {
+  const { authors, book } = res.locals
+  if (authors > 1) {
+    const authorBooks = await Promise.all(
+      authors.map(async (author) => {
+        let book = await AuthorBooksModel.getAuthorBook(author.id, book.id)
+        if (!book) {
+          book = await AuthorBooksModel.addBook(author.id, book.id)
+          return book
+        }
+        return book
+      })
+    )
+    res.locals.authorBooks = authorBooks
+    next()
+  } else {
+    let bookCheck = await AuthorBooksModel.getAuthorBook(authors.id, book.id)
+    if (!bookCheck) {
+      bookCheck = await AuthorBooksModel.addBook(authors.id, book.id)
+    }
+    res.locals.authorBooks = bookCheck
+    next()
+  }
+}
+
+module.exports = {
+  verifyBody,
+  verifyBook,
+  verifyAuthors,
+  verifyAuthorBook
+}
