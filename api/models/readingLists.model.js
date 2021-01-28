2312a03bc7d9dd88ab7736769ae19d882292d203
Cody Brewer <codybrewerdev@gmail.com>
2021-01-28 12:37:01 -0700

    feat(readingLists.model.js): database functions for managing reading lists in the reading_lists table and to get reading lists information

--- api/models/readingLists.model.js
+++ api/models/readingLists.model.js
@@ -1,30 +1,32 @@
-const db = require('../../config/db');
+const db = require('../../config/db')
 
-const create = (list) => db('reading_lists').insert(list).returning('*');
+const create = (list) => db('reading_lists').insert(list).returning('*')
 
-const getAll = () => db('reading_lists').select('*');
+const getAll = () => db('reading_lists').select('*')
 
-const getAllBy = (filter) => db('reading_lists').where(filter).select('*');
+const getAllBy = (filter) => db('reading_lists').where(filter).select('*')
 
-const getById = (id) => db('reading_lists').where({ id }).select('*');
+const getById = (id) => db('reading_lists').where({ id }).select('*')
 
 const update = (id, changes) => {
-  db('reading_lists').where({ id }).update(changes).returning('*');
-};
+  db('reading_lists').where({ id }).update(changes).returning('*')
+}
 
 const remove = (id) => {
-  db('reading_lists').where({ id }).first().delete();
-};
+  db('reading_lists').where({ id }).first().delete()
+}
 
-const getListBooks = (listId) => db('reading_list_books')
-  .join('books', 'reading_list_books.book_id', 'books.id')
-  .where({ reading_list_id: listId })
-  .select('books.*');
+const getListBooks = (listId) =>
+  db('reading_list_books')
+    .join('books', 'reading_list_books.book_id', 'books.id')
+    .where({ reading_list_id: listId })
+    .select('books.*')
 
-const getBooksAuthors = (bookId) => db('author_books')
-  .where({ book_id: bookId })
-  .join('authors', 'authors.id', 'author_books.author_id')
-  .select('authors.name');
+const getBooksAuthors = (bookId) =>
+  db('author_books')
+    .where({ book_id: bookId })
+    .join('authors', 'authors.id', 'author_books.author_id')
+    .select('authors.name')
 
 module.exports = {
   create,
@@ -34,5 +36,5 @@ module.exports = {
   getListBooks,
   getBooksAuthors,
   update,
-  remove,
-};
+  remove
+}
