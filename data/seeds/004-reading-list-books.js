b1c1ffb93c6b35671ef23f3eaf5a9597defe9bd6
Cody Brewer <codybrewerdev@gmail.com>
2021-01-29 11:30:22 -0700

    fix(data/seeds/): remove id column from data in seeds

--- data/seeds/004-reading-list-books.js
+++ data/seeds/004-reading-list-books.js
@@ -5,13 +5,14 @@ exports.seed = (knex) =>
     .then(() =>
       // Inserts seed entries
       knex('reading_list_books').insert([
-        { reading_list_id: 0, book_id: 0 },
         { reading_list_id: 1, book_id: 1 },
         { reading_list_id: 2, book_id: 2 },
         { reading_list_id: 3, book_id: 3 },
-        { reading_list_id: 4, book_id: 1 },
+        { reading_list_id: 4, book_id: 4 },
         { reading_list_id: 5, book_id: 2 },
         { reading_list_id: 6, book_id: 2 },
         { reading_list_id: 7, book_id: 1 },
         { reading_list_id: 8, book_id: 3 },
-      ]));
+        { reading_list_id: 9, book_id: 4 }
+      ])
+    )
--- data/seeds/005-authors.js
+++ data/seeds/005-authors.js
@@ -1,8 +1,11 @@
-exports.seed = (knex) => knex('authors')
-  .del()
-  .then(() => knex('authors').insert([
-    { id: 0, name: 'William Shakespeare' },
-    { id: 1, name: 'Herman Melville' },
-    { id: 2, name: 'Mark Adams' },
-    { id: 3, name: 'Raymond Chandler' },
-  ]));
+exports.seed = (knex) =>
+  knex('authors')
+    .del()
+    .then(() =>
+      knex('authors').insert([
+        { name: 'William Shakespeare' },
+        { name: 'Herman Melville' },
+        { name: 'Mark Adams' },
+        { name: 'Raymond Chandler' }
+      ])
+    )
