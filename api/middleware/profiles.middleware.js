6a76ed04ae3f9475d983ee6dd021654dfd0b847b
Cody Brewer <codybrewerdev@gmail.com>
2021-02-01 13:13:50 -0700

    feat(profiles.middleware.js): middleware to check when getting a single profile that it exists and is public

---
+++ api/middleware/profiles.middleware.js
@@ -0,0 +1,34 @@
+const UserModel = require('../models/user.model')
+
+const verifyProfile = async (req, res, next) => {
+  console.log({ requestedUserId: req.params.profileId })
+  try {
+    // check if profile exists
+    const userProfile = await UserModel.getUserBy({
+      uuid: req.params.profileId
+    })
+    console.log({ userProfile })
+    // if not send 404 profile does not exists
+    if (userProfile == null) {
+      const error = new Error('User Profile does not exist')
+      res.status(404)
+      next(error)
+    }
+    // check if profile is set to public
+    // if not send 403 forbidden user is not public
+    if (userProfile.public === false) {
+      const error = new Error('User profile is not public')
+      res.status(403)
+      next(error)
+    }
+    // if profile exists and is public set res.locals.userProfile to the profile
+    res.locals.profile = userProfile
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  verifyProfile
+}
