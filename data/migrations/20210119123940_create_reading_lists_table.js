0f18ce00b0f8e1b81401862e0a46784736ff2539
Cody Brewer <codybrewerdev@gmail.com>
2021-02-01 10:57:43 -0700

    fix migrations after rebase

--- data/migrations/20210119123940_create_reading_lists_table.js
+++ data/migrations/20210119123940_create_reading_lists_table.js
@@ -1,10 +1,10 @@
 exports.up = (knex) =>
   knex.schema.createTable('reading_lists', (table) => {
-    table.increments('id');
-    table.uuid('user_id').unsigned().index().references('uuid').inTable('users');
-    table.text('name').notNullable();
-  });
+    table.increments('id')
+    table.uuid('user_id').unsigned().index().references('uuid').inTable('users')
+    table.text('name').notNullable()
+  })
 
 exports.down = (knex) => {
-  knex.schema.dropTableIfExists('reading_lists');
-};
+  knex.schema.dropTableIfExists('reading_lists')
+}
