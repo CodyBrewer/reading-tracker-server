863ae7b5d5208e2cd30eb4e9342b02ae2a2999d4
Cody Brewer <codybrewerdev@gmail.com>
2021-02-02 09:46:06 -0700

    route to get single profile

--- api/routes/profiles.router.js
+++ api/routes/profiles.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const UserModel = require('../models/user.model')
 const { verifyToken } = require('../middleware/authentication.middleware')
 const { verifyProfile } = require('../middleware/profiles.middleware')
-
+const readingListRouter = require('./readingLists.router')
 /**
  * @swagger
  * components:
@@ -14,9 +14,13 @@ const { verifyProfile } = require('../middleware/profiles.middleware')
  *          type: string
  *        uuid:
  *          type: string
+ *        avatar_url:
+ *          type: string
+ *          format: url
  *      example:
  *        username: The_Riker
  *        uuid: 7a97e42c-124c-4e2c-8109-c5ce6e5f77a4
+ *        avatar_url: https://upload.wikimedia.org/wikipedia/en/thumb/2/20/WilRiker.jpg/220px-WilRiker.jpg
  *
  * /profiles:
  *   get:
@@ -59,8 +63,39 @@ router.get('/', verifyToken, async (req, res, next) => {
   }
 })
 
-router.get('/:profileId', verifyToken, verifyProfile, async (req, res) => {
-  res.status(200).json(res.locals.profile)
+/**
+ * @swagger
+ * components:
+ *  parameters:
+ *    ProfileId:
+ *      in: path
+ *      name: profileId
+ *      description: Public UUID of profile to request
+ *      schema:
+ *        type: string
+ *      required: true
+ *      example: '1d9dd170-8757-40ec-9ccf-11e4e3de27b1'
+ * /profiles/:profileId/:
+ *  get:
+ *    description: Get profile information for one profile
+ *    tags:
+ *      - profiles
+ *    parameters:
+ *      - $ref: '#/components/parameters/ProfileId'
+ *    responses:
+ *      200:
+ *        description: Information for the Profile requested
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/PublicProfile'
+ *      401:
+ *        $ref: '#/components/respones/UnauthorizedError'
+ *      403:
+ *        description: 'User Profile is not public'
+ */
+router.get('/:profileId', verifyToken, verifyProfile, (req, res) => {
+  res.status(200).json(res.locals.otherProfile)
 })
 
 module.exports = router
