a6ae2e58b4cb30a7558ee4bbfe48dea9bdc24dcb
Cody Brewer <codybrewerdev@gmail.com>
2021-02-02 16:59:27 -0700

    feat(createBookObject): db function to create book object with books authors

--- api/models/books.model.js
+++ api/models/books.model.js
@@ -4,14 +4,26 @@ const getBy = (filter) => db('books').where(filter).select('*')
 
 const create = (book) => db('books').insert(book).returning('*')
 
-const update = (id, changes) =>
-  db('reading_lists').where({ id }).update(changes).returning('*')
+const update = (id, changes) => db('reading_lists').where({ id }).update(changes).returning('*')
 
 const remove = (id) => db('books').where({ id }).first().delete()
+
+const getBooksAuthors = (bookId) =>
+  db('author_books')
+    .where({ book_id: bookId })
+    .join('authors', 'authors.id', 'author_books.author_id')
+    .select('authors.name')
+
+const createBookObject = async (book) => {
+  const booksAuthors = await getBooksAuthors(book.id)
+  const authors = booksAuthors.map((author) => author.name)
+  return { ...book, authors }
+}
 
 module.exports = {
   getBy,
   create,
   update,
-  remove
+  remove,
+  createBookObject
 }
