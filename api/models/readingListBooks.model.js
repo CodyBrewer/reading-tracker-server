ca4672bb55b0a9dedc677583633e0dd28ba06a02
Cody Brewer <codybrewerdev@gmail.com>
2021-02-02 13:32:14 -0700

    feat(changeReadingList): db fucntion to change what book a reading list belongs to

--- api/models/readingListBooks.model.js
+++ api/models/readingListBooks.model.js
@@ -2,14 +2,22 @@ const db = require('../../config/db')
 
 const getBy = (filter) => db('reading_list_books').where(filter).select('*')
 
-const getByBookId = (book_id) =>
-  db('reading_list_books').where({ book_id }).first().select('*')
+const getByBookId = (book_id) => db('reading_list_books').where({ book_id }).first().select('*')
 
 const addBook = (book_id, reading_list_id) =>
   db('reading_list_books').insert({ book_id, reading_list_id }).select('*')
 
+const changeReadingList = async (book_id, reading_list_id, to_list_id) => {
+  const [bookListItem] = await getBy({ book_id, reading_list_id })
+  return db('reading_list_books')
+    .where({ id: bookListItem.id })
+    .update({ reading_list_id: to_list_id })
+    .returning('*')
+}
+
 module.exports = {
   addBook,
   getBy,
-  getByBookId
+  getByBookId,
+  changeReadingList
 }
