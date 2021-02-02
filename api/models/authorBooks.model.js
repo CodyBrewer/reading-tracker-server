b035151421b281834d2b3c86c2da679351e6c6b9
Cody Brewer <codybrewerdev@gmail.com>
2021-02-02 14:54:41 -0700

    fix(getAuthorBook): fix no return

--- api/models/authorBooks.model.js
+++ api/models/authorBooks.model.js
@@ -1,12 +1,10 @@
 const db = require('../../config/db')
 
 const addBook = (authorId, bookId) =>
-  db('author_books')
-    .insert({ author_id: authorId, book_id: bookId })
-    .returning('*')
+  db('author_books').insert({ author_id: authorId, book_id: bookId }).returning('*')
 
 const getAuthorBook = (author_id, book_id) => {
-  db('author_books').where({ author_id, book_id }).first().select('*')
+  return db('author_books').where({ author_id, book_id }).first().select('*')
 }
 
 module.exports = {
