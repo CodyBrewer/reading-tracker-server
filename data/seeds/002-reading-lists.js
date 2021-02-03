478d31ef74a285481eb7cee9b2be5e3394ad2bd4
Cody Brewer <codybrewerdev@gmail.com>
2021-02-02 17:28:28 -0700

    fix(002-reading-lists): change cmdr data's reading lists to private

--- data/seeds/002-reading-lists.js
+++ data/seeds/002-reading-lists.js
@@ -11,8 +11,8 @@ exports.seed = (knex) =>
         { user_id: '7a97e42c-124c-4e2c-8109-c5ce6e5f77a4', name: 'read' },
         { user_id: '7a97e42c-124c-4e2c-8109-c5ce6e5f77a4', name: 'to-read' },
         { user_id: '7a97e42c-124c-4e2c-8109-c5ce6e5f77a4', name: 'reading' },
-        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'read' },
-        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'to-read' },
-        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'reading' }
+        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'read', public: false },
+        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'to-read', public: false },
+        { user_id: '1e4d861c-d301-4318-9fd6-96ccbec9f821', name: 'reading', public: false }
       ])
     )
