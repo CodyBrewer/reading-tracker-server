2bbb6d4c05d2a546c2a81c50b6526bebbb41d79e
Cody Brewer <codybrewerdev@gmail.com>
2021-02-06 14:08:42 -0700

    fix(user.model.js): remove returning avatar_url column for getUserBy and getAllPublic functions

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,16 +1,10 @@
 const db = require('../../config/db')
 
-const getUserPassword = (filter) =>
-  db('users').where(filter).first().select('uuid', 'password')
+const getUserPassword = (filter) => db('users').where(filter).first().select('uuid', 'password')
 
-const getUserBy = (filter) =>
-  db('users')
-    .where(filter)
-    .first()
-    .select('uuid', 'username', 'avatar_url', 'public')
+const getUserBy = (filter) => db('users').where(filter).first().select('uuid', 'username', 'public')
 
-const getAllPublic = () =>
-  db('users').where({ public: true }).select('uuid', 'username', 'avatar_url')
+const getAllPublic = () => db('users').where({ public: true }).select('uuid', 'username')
 
 const create = async (user) => {
   const inserted = await db('users').insert(user)
